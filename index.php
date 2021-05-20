d331f6e75acbc32ddf24ec56c1ba0581dc6c699e
jshensh <admin@imjs.work>
2021-05-20 12:33:13 +0800

    兼容大文件下载

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require('./vendor/autoload.php');
 use CustomCurl\Client;
 
 $originProtocol = 'http';
-$originSite = 'baidu.com';
-$thisSite   = 'example.com';
+$originSite     = 'baidu.com';
+$thisSite       = 'example.com';
 
 if (!function_exists('getallheaders')) {
     function getallheaders() {
@@ -18,63 +18,144 @@ if (!function_exists('getallheaders')) {
     }
 }
 
-$curlObj0 = Client::init("{$originProtocol}://{$originSite}{$_SERVER['REQUEST_URI']}", $_SERVER['REQUEST_METHOD'])
-                ->setCurlOpt(CURLOPT_ENCODING, '');
+class ReverseProxy
+{
+    private $flags = [
+            'FILENAME_SENT'       => false,
+            'HEADER_SENT'         => false,
+            'CURL_DOWNLOAD_FILE'  => false,
+            'CURL_VALID_RESPONSE' => true
+        ];
+    private $runtimeData = [
+            'headerSize'       => -1,
+            'outputBuffer'     => [
+                'header' => [],
+                'body'   => ''
+            ]
+        ];
+    private $config = [
+            'replace' => null,
+        ];
 
-$headers = getallheaders();
+    public function curlCallback($ch, $data)
+    {
+        $info = curl_getinfo($ch);
 
-foreach ($headers as $key => $value) {
-    $keyArr = ['accept-encoding', 'host', 'referer', 'cookie', 'content-type', 'user-agent', 'content-length'];
-    if (in_array(strtolower($key), $keyArr)) {
-        continue;
-    }
-    $curlObj0 = $curlObj0->setHeader($key, str_replace($thisSite, $originSite, $value));
-}
+        if ($this->runtimeData['headerSize'] < $info['header_size']) {
+            $this->runtimeData['headerSize'] = $info['header_size'];
 
-if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']) {
-    $curlObj0 = $curlObj0->set('referer', str_replace($thisSite, $originSite, $_SERVER['HTTP_REFERER']));
-}
-
-if (isset($_SERVER['HTTP_COOKIE']) && $_SERVER['HTTP_COOKIE']) {
-    $curlObj0 = $curlObj0->setCookies($_SERVER['HTTP_COOKIE']);
-}
-
-if (in_array($_SERVER['REQUEST_METHOD'], ['POST', 'PUT', 'DELETE'])) {
-    $curlObj0 = $curlObj0->set('postType', 'string')
-                    ->set('postFields', file_get_contents("php://input"));
-
-    if (isset($_SERVER['HTTP_CONTENT_TYPE']) && $_SERVER['HTTP_CONTENT_TYPE']) {
-        $curlObj0 = $curlObj0->setHeader('Content-Type', $_SERVER['HTTP_CONTENT_TYPE']);
-    }
-}
-
-$curlObj0 = $curlObj0->exec();
-
-if ($curlObj0->getStatus()) {
-    $headers = explode("\r\n", $curlObj0->getHeader());
-    foreach ($headers as $header) {
-        if (!$header || strpos(strtolower($header), 'content-length:') > -1 || strpos(strtolower($header), 'content-encoding:') > -1) {
-            continue;
+            if (!$this->flags['HEADER_SENT']) {
+                if (strpos(strtolower($data), 'content-disposition: attachment') !== false) {
+                    $this->flags['FILENAME_SENT'] = true;
+                    $this->flags['CURL_DOWNLOAD_FILE'] = true;
+                }
+                $this->runtimeData['outputBuffer']['header'][] = $data;
+            }
+            // header 结束
+            if ($data === "\r\n") {
+                $this->runtimeData['headerSize'] += 2;
+                if (!$this->flags['HEADER_SENT']) {
+                    foreach ($this->runtimeData['outputBuffer']['header'] as $header) {
+                        if (strpos(strtolower($header), 'content-encoding:') !== false) {
+                            continue;
+                        }
+                        if ($this->config['replace']) {
+                            header(str_replace($this->config['replace'][0], $this->config['replace'][1], $header), false);
+                        } else {
+                            header($header, false);
+                        }
+                    }
+                    if (!$this->flags['FILENAME_SENT'] && $this->flags['CURL_DOWNLOAD_FILE']) {
+                        header('Content-Disposition: attachment; filename=' . basename($this->config['filePath']));
+                    }
+                    $this->flags['HEADER_SENT'] = true;
+                }
+            }
+        } else {
+            // Data 段数据处理
+            if ($this->flags['CURL_DOWNLOAD_FILE']) {
+                echo $data;
+                ob_flush();
+                flush();
+            } else {
+                $this->runtimeData['outputBuffer']['body'] .= $data;
+            }
         }
-        header(str_replace($originSite, $thisSite, $header), false);
+
+        return strlen($data);
     }
 
-    // 输出关键词替换
-    $body = str_replace(
+    public function __construct($config)
+    {
+        $this->config = array_merge($this->config, $config);
+
+        $curlObj = Client::init("{$this->config['originProtocol']}://{$this->config['originSite']}{$_SERVER['REQUEST_URI']}", $_SERVER['REQUEST_METHOD'])
+            ->setCurlOpt(CURLOPT_ENCODING, '')
+            ->set('timeout', 0)
+            ->set('reRequest', 1)
+            ->set('followLocation', 0);
+
+        $headers = getallheaders();
+
+        foreach ($headers as $key => $value) {
+            $keyArr = ['accept-encoding', 'host', 'referer', 'cookie', 'user-agent', 'content-length'];
+            if (in_array(strtolower($key), $keyArr)) {
+                continue;
+            }
+            $curlObj = $curlObj->setHeader($key, str_replace($this->config['thisSite'], $this->config['originSite'], $value));
+        }
+
+        if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']) {
+            $curlObj = $curlObj->set('referer', str_replace($this->config['thisSite'], $this->config['originSite'], $_SERVER['HTTP_REFERER']));
+        }
+
+        if (isset($_SERVER['HTTP_COOKIE']) && $_SERVER['HTTP_COOKIE']) {
+            $curlObj = $curlObj->setCookies(str_replace($this->config['thisSite'], $this->config['originSite'], $_SERVER['HTTP_COOKIE']));
+        }
+
+        if (in_array($_SERVER['REQUEST_METHOD'], ['POST', 'PUT', 'DELETE'])) {
+            $curlObj = $curlObj->set('postType', 'string')
+                ->set('postFields', file_get_contents("php://input"));
+        }
+
+        $res = $curlObj->setCurlOpt(CURLOPT_WRITEFUNCTION, [$this, 'curlCallback'])->exec();
+
+        if ($res->getStatus()) {
+            if (!$this->flags['CURL_DOWNLOAD_FILE']) {
+                if ($this->config['replace']) {
+                    $this->runtimeData['outputBuffer']['body'] = str_replace($this->config['replace'][0], $this->config['replace'][1], $this->runtimeData['outputBuffer']['body']);
+                }
+
+                foreach ($this->runtimeData['outputBuffer']['header'] as $header) {
+                    if (strpos(strtolower($header), 'content-length:') !== false) {
+                        header('Content-Length: ' . strlen($this->runtimeData['outputBuffer']['body']), false);
+                        continue;
+                    }
+                    if (strpos(strtolower($header), 'content-encoding:') !== false) {
+                        continue;
+                    }
+                    if ($this->config['replace']) {
+                        header(str_replace($this->config['replace'][0], $this->config['replace'][1], $header), false);
+                    } else {
+                        header($header, false);
+                    }
+                }
+
+                echo $this->runtimeData['outputBuffer']['body'];
+            }
+        }
+    }
+}
+
+new ReverseProxy([
+    'replace' => [
         [
             $originSite
         ], [
             $thisSite
-        ], $curlObj0->getBody()
-    );
-
-    header('Content-Length: ' . strlen($body));
-    echo $body;
-
-    // 调试信息
-    // echo '<pre style="text-align: left;">';
-    // var_dump($curlObj0->getHeader(), $curlObj0->getCookies(), $_POST, str_replace($thisSite, $originSite, $_SERVER['HTTP_REFERER']), $_COOKIE);
-    // echo '</pre>';
-} else {
-    var_dump($curlObj0->getCurlErrNo());
-}
+        ]
+    ],
+    'originProtocol' => $originProtocol,
+    'originSite' =>$originSite,
+    'thisSite' => $thisSite
+]);
